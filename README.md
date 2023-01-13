b0c33281ca577497d957700f3c0a6960bb428e3a
Alden Bradford <aldenbradford@gmail.com>
2023-01-13 14:52:30 -0500

    use a simpler protocol for faster detection

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # DoorCounter
 A IR beam detector for counting when people enter or exit a room, running on a Raspberry Pi Pico.
-
-This relies on Peter Hinch's [micropython_ir library](https://github.com/peterhinch/micropython_ir) (thanks Peter!).
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from machine import Pin
-from ir_rx.nec import NEC_8
-from ir_tx.nec import NEC
 import time
+import rp2
 
-tic = time.ticks_ms()
 
-def make_callback(label):
-    def callback(data, addr, ctrl):
-        toc = time.ticks_ms() - tic
-        if data < 0:  # NEC protocol sends repeat codes.
-            print(f'{label} {toc} Repeat code.')
-        else:
-            print(f'{label} {toc} Data {data:02x} Addr {addr:04x}')
-    return callback
-    
-        
-sensor = [NEC_8(Pin(x, Pin.IN), make_callback(x)) for x in [15, 14]]
+@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW)
+def drone():
+    # datasheet specifies relaxation time >= 5ms
+    # total instructions: 2 + 4(2 + 32*8) = 1034
+    # 1034 instructions / 76k instr/sec = 13.6 ms
+    set(y, 4)  # 1
+    label("blank")
+    set(x, 31)  # 4 * 1
+    label("blank_inner")
+    jmp(x_dec, "blank_inner").delay(7)  # 4*32*8
+    jmp(y_dec, "blank")  # 4 * 1
 
-transmitter = NEC(Pin(8, Pin.OUT, value=0))
+    # datasheet specifies at least 10 pulses per burst
+    # NEC protocol uses 21 pulse and 237 pulse bursts
+    set(x, 31)  # 1
+    label("burst")
+    nop().side(1)
+    jmp(x_dec, "burst").side(0)
+    # 32 pulses / 38khz = 842 us
 
+
+beacon = rp2.StateMachine(0, drone, freq=2 * 38_000, sideset_base=Pin(8))
+beacon.active(True)
+
+sense = Pin(15, Pin.IN)
+onboard_led = Pin(25, Pin.OUT)
 while True:
-    time.sleep(0.2)
-    print()
-    tic = time.ticks_ms()
-    transmitter.transmit(0x27, 0x18)
-    print(time.ticks_ms() - tic)
-    
+    onboard_led.value(not sense.value())
